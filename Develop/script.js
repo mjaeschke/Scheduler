4ab6d306eee25c5c76e79b07fd801bafecbcd23c
Michael Jaeschke <jaeschke21@gmail.com>
2020-05-16 14:52:24 -0500

    created the time

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,11 @@
 
 var dates = $("#currentDay");
 var formatedDate = moment().format('MMMM Do YYYY, h:mm a');
+console.log("today is: "+ formatedDate);
 var cdElm = $("<p>").text("today is: " + formatedDate);
 dates.append(cdElm);
+var currentHour =  moment().format("h:mm a");
+console.log("the current hour is: "+ currentHour);
 
-console.log("today is: "+ formatedDate);
-
+  
     
