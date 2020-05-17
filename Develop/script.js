0165db691a2efa009688eb1cfd8e12a527a95320
Michael Jaeschke <jaeschke21@gmail.com>
2020-05-16 21:32:06 -0500

    changed some of the css files and mad a loop that makes all the html elements and changes color if its current past or future times

--- Develop/script.js
+++ Develop/script.js
@@ -8,4 +8,38 @@ var currentHour =  moment().format("h:mm a");
 console.log("the current hour is: "+ currentHour);
 
   
-    
+var workHours = [
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm"
+];
+
+
+var hoursDiv = $("#buisnessHours");
+var descriptionDiv = $("#currentHour");
+var saveDiv = $("#saveBtn");
+
+
+for (var i = 0; i < workHours.length; i++) {
+  
+  var workHoursNew = $("<p>" + workHours[i] + "</p>").addClass("hour");
+  var descriptionNew = $("<input size=90>"+"</input>").addClass("description");
+  var savBtnNew = $("<button>"+"save"+"</button>").addClass("saveBtn");
+  hoursDiv.append(workHoursNew);
+  saveDiv.append(savBtnNew);
+  if(workHours[i] === currentHour){
+    descriptionDiv.append(descriptionNew).addClass("present");
+  console.log("this is in the past")
+  }else if(workHours[i]<currentHour){
+    descriptionDiv.append(descriptionNew).addClass("past");
+  console.log("this is in the past")
+  }else if (workHours[i]>currentHour){
+    descriptionDiv.append(descriptionNew).addClass("future");
+  }
+}
